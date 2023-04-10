4e7afb7f57d9d9f03205b569dba93413ffb13ba9
Bhimgouda <satvikp508@gmail.com>
2023-04-10 14:24:05 +0530

    added slug generator

--- utils/convertToSlug.js
+++ utils/convertToSlug.js
@@ -1,15 +1,6 @@
+var slug = require("slug");
+
 module.exports = function convertToSlug(title) {
-    // Replace spaces with dashes
-    let slug = title.replace(/\s+/g, '-');
-  
-    // Replace non-alphanumeric characters with dashes
-    slug = slug.replace(/[^a-zA-Z0-9-]/g, '-');
-  
-    // Make the string lowercase
-    slug = slug.toLowerCase();
-  
-    // Remove any dashes at the beginning or end of the string
-    slug = slug.replace(/^-+|-+$/g, '').trim();
-  
-    return slug;
-  }
+  const generatedSlug = slug(title);
+  return generatedSlug;
+};
